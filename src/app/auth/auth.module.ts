8f13e5389a88e896507536ee6bed86656018c2b2
Jorge Blanchard Cruz <rothgard@live.com>
2024-04-28 13:23:35 +0100

    next

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { LogingPageComponent } from './pages/loging-page/loging-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-
 
 
 @NgModule({
   declarations: [
     LayoutPageComponent,
-    LogingPageComponent,
     LoginPageComponent,
     RegisterPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class AuthModule { }

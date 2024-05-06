9e22c5984926c3ba7ac3b874ad0cf1cbc490c420
Jorge Blanchard Cruz <rothgard@live.com>
2024-05-06 18:51:28 +0100

    auth guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 import { enviroments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
@@ -27,9 +27,25 @@ export class AuthService {
 
     return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
-        tap( user => this.user = user),
-        tap( user => localStorage.setItem(this.token, 'ieoapsodpo.weo02opas0.29iaoksd'))
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem(this.token, 'ieoapsodpo.weo02opas0.29iaoksd') )
       );
+  }
+
+  public checkAuth(): Observable<boolean> | boolean {
+
+    if (!localStorage.getItem(this.token))
+      return false;
+
+    const token = localStorage.getItem(this.token);
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( error => of(false) )
+      );
+
   }
 
   public logout(): void {
